2b268bccfb9393661761273cc320934cf169d095
solsticenet <solsticenet@mm1.myfiosgateway.com>
2020-11-26 20:18:55 -0500

    rename interface

--- go101doc.go
+++ go101doc.go
@@ -14,8 +14,8 @@ import (
 	"sync"
 )
 
-// GoDOC -Build a Go 101 ebook in the current directory.
-type GoDOC interface {
+// go101DocMaker builds a Go 101 ebook in the current directory.
+type go101DocMaker interface {
 	pageRequest(url string) []byte
 	setLinks(bookLinks [][]byte)
 	getLinks() [][]byte
@@ -24,14 +24,13 @@ type GoDOC interface {
 	gzipWrite(path []byte, out chan<- struct{}, lock *sync.Mutex)
 }
 
-type docMaker struct {
-	GoDOC
+type go101Doc struct {
 	baseURL string
 	links   [][]byte
 	buf     bytes.Buffer
 }
 
-func (doc docMaker) pageRequest(endpoint string) []byte {
+func (doc go101Doc) pageRequest(endpoint string) []byte {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", doc.baseURL+endpoint, nil)
 	response, err := client.Do(req)
@@ -43,15 +42,15 @@ func (doc docMaker) pageRequest(endpoint string) []byte {
 	return rText
 }
 
-func (doc *docMaker) setLinks(bookLinks [][]byte) {
+func (doc *go101Doc) setLinks(bookLinks [][]byte) {
 	doc.links = bookLinks
 }
 
-func (doc *docMaker) getLinks() [][]byte {
+func (doc *go101Doc) getLinks() [][]byte {
 	return doc.links
 }
 
-func (doc *docMaker) gzipWrite(path []byte, out chan<- struct{}, lock *sync.Mutex) {
+func (doc *go101Doc) gzipWrite(path []byte, out chan<- struct{}, lock *sync.Mutex) {
 	resp := doc.pageRequest(string(path))
 	var respMinusFooter []byte
 	for i := range resp {
@@ -72,7 +71,7 @@ func (doc *docMaker) gzipWrite(path []byte, out chan<- struct{}, lock *sync.Mute
 	out <- struct{}{}
 }
 
-func (doc *docMaker) getBookData() {
+func (doc *go101Doc) getBookData() {
 	ch := make(chan struct{})
 	var mutex = &sync.Mutex{}
 	for _, urlPath := range doc.links {
@@ -83,16 +82,16 @@ func (doc *docMaker) getBookData() {
 	}
 }
 
-func (doc *docMaker) getDecompBuffer() []byte {
+func (doc *go101Doc) getDecompBuffer() []byte {
 	readComp, _ := gzip.NewReader(&doc.buf)
 	out, _ := ioutil.ReadAll(readComp)
 	return out
 }
 
 func main() {
-	var goDOC GoDOC
-	goDOC = &docMaker{baseURL: "https://go101.org/article/"}
-	mainPage := goDOC.pageRequest("101.html")
+	var goDoc go101DocMaker
+	goDoc = &go101Doc{baseURL: "https://go101.org/article/"}
+	mainPage := goDoc.pageRequest("101.html")
 	foundLinks := make([][]byte, 0)
 	for _, line := range bytes.Split(mainPage, []byte("\n")) {
 		if bytes.Contains(line, []byte("<li")) && bytes.Contains(line, []byte("index")) {
@@ -100,9 +99,9 @@ func main() {
 			foundLinks = append(foundLinks, link)
 		}
 	}
-	goDOC.setLinks(foundLinks)
-	goDOC.getBookData()
-	bookData := bytes.Split(goDOC.getDecompBuffer(), []byte("|+|"))
+	goDoc.setLinks(foundLinks)
+	goDoc.getBookData()
+	bookData := bytes.Split(goDoc.getDecompBuffer(), []byte("|+|"))
 	cwd, _ := os.Getwd()
 	path := cwd + string(os.PathSeparator) + "go101.html"
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -111,7 +110,7 @@ func main() {
 		os.Exit(1)
 	}
 	defer f.Close()
-	for _, pageName := range goDOC.getLinks() {
+	for _, pageName := range goDoc.getLinks() {
 		for _, pageData := range bookData {
 			current := pageData[:len(pageName)]
 			if bytes.Compare(pageName, current) != 0 {
